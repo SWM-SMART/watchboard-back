27b8841d84df7187da7f127e72cd8c9e82c6c1d9
kms <noparamin@naver.com>
2023-07-23 16:15:11 +0900

    :construction_worker: chore: annotate SpringBootTest annotation

--- src/test/java/com/smart/watchboard/WatchboardApplicationTests.java
+++ src/test/java/com/smart/watchboard/WatchboardApplicationTests.java
@@ -3,7 +3,7 @@ package com.smart.watchboard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WatchboardApplicationTests {
 
 	@Test
