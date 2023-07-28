ee20a9c196e416bd0be51a4470e9963fcb73f270
kms <noparamin@naver.com>
2023-07-29 00:25:34 +0900

    :hammer: fix: modify api path from /users to /users/info

--- src/main/java/com/smart/watchboard/controller/UserController.java
+++ src/main/java/com/smart/watchboard/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 @Tag(name = "사용자 API", description = "사용자 관련 API(mock)")
 public class UserController {
-    @GetMapping()
+    @GetMapping("/info")
     @Operation(summary = "사용자 정보 조회", description = "요청받은 사용자의 정보를 조회한다.")
     public ResponseEntity<?> getUserInformation(@RequestHeader("accessToken") String accessToken) {
         UserInformationDto userInformationDto = new UserInformationDto(1L, "권민석", "noparamin@naver.com");
