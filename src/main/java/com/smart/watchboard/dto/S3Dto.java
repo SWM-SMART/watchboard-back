0f00bc9ac0e68950e8508b0fe259da54dfa0bea8
kms <noparamin@naver.com>
2023-10-19 10:33:24 +0900

    :sparkles: feat: create pdf features

---
+++ src/main/java/com/smart/watchboard/dto/S3Dto.java
@@ -0,0 +1,21 @@
+package com.smart.watchboard.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class S3Dto {
+    private MultipartFile file;
+    private Long documentId;
+    private Long fileId;
+
+    public S3Dto(MultipartFile file, long documentId, Long fileId) {
+        this.file = file;
+        this.documentId = documentId;
+        this.fileId = fileId;
+    }
+}
