7f198fafc03ac56819006e2648b200c7204dc64d
kms <noparamin@naver.com>
2023-11-07 00:53:58 +0900

    :sparkles: feat: create request ai answer feature #36

--- src/main/java/com/smart/watchboard/service/MindmapService.java
+++ src/main/java/com/smart/watchboard/service/MindmapService.java
@@ -27,6 +27,7 @@ public class MindmapService {
 
     private final MindmapRepository mindmapRepository;
     private final WhiteboardService whiteboardService;
+    private final RequestService requestService;
 
     public void createMindmap(ResponseEntity<String> responseEntity, Long documentId) throws JsonProcessingException {
         Document document = whiteboardService.findDoc(documentId);
@@ -77,5 +78,9 @@ public class MindmapService {
         updatedMindmap.setKeywords(newKeywords);
 
         mindmapRepository.save(updatedMindmap);
+    }
+
+    public ResponseEntity<String> getKeywordAnswer(Long documentId, String keywordLabel) throws JsonProcessingException {
+        return requestService.requestAnswer(documentId, keywordLabel);
     }
 }
