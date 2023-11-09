8c2ed368ad7db6e1b1378b15ca10c6992c31b16c
kms <noparamin@naver.com>
2023-11-09 11:08:06 +0900

    :sparkles: feat: create keywordsbodyDto #40

---
+++ src/main/java/com/smart/watchboard/dto/KeywordsBodyDto.java
@@ -0,0 +1,13 @@
+package com.smart.watchboard.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class KeywordsBodyDto {
+    private List<String> keywords;
+
+}
