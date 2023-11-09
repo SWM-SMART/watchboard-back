4400f5a9a06461509d04ad07411a2b2ef4f72e70
kms <noparamin@naver.com>
2023-11-09 14:08:27 +0900

    :sparkles: feat: create keyword repo #43

---
+++ src/main/java/com/smart/watchboard/repository/KeywordRepository.java
@@ -0,0 +1,11 @@
+package com.smart.watchboard.repository;
+
+import com.smart.watchboard.domain.Keyword;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface KeywordRepository extends MongoRepository<Keyword, ObjectId> {
+    Optional<Keyword> findByDocumentId(Long documentId);
+}
