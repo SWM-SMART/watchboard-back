5abede3e3d82638d7bab99d202d15899f52e52ae
kms <noparamin@naver.com>
2023-11-09 10:41:15 +0900

    :hammer: fix: add set properties in updateFile #40

--- src/main/java/com/smart/watchboard/service/RequestService.java
+++ src/main/java/com/smart/watchboard/service/RequestService.java
@@ -7,10 +7,7 @@ import com.smart.watchboard.domain.Summary;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
