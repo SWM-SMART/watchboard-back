1d00359c5c8e438d1e0d75835d8292d561a476b5
kms <noparamin@naver.com>
2023-11-11 22:27:30 +0900

    :hammer: feat: change String to dto #45

--- src/main/java/com/smart/watchboard/service/RequestService.java
+++ src/main/java/com/smart/watchboard/service/RequestService.java
@@ -1,10 +1,8 @@
 package com.smart.watchboard.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.smart.watchboard.domain.Summary;
-import com.smart.watchboard.dto.KeywordsBodyDto;
+import com.smart.watchboard.dto.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 
@@ -36,24 +35,21 @@ public class RequestService {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
 
-        // 요청 본문 추가
-        String requestBody = """
-                {
-                  "dbInfo": {
-                    "key": "%s",
-                    "db": "s3"
-                  }
-                }
-                """.formatted(fileName);
-
-        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+//        // 요청 본문 추가
+//        String requestBody = """
+//                {
+//                  "dbInfo": {
+//                    "key": "%s",
+//                    "dataType": "pdf"
+//                  }
+//                }
+//                """.formatted(fileName);
+        RequestBodyToServerDto requestBodyToServerDto = new RequestBodyToServerDto(fileName, "audio");
+        HttpEntity<RequestBodyToServerDto> requestEntity = new HttpEntity<>(requestBodyToServerDto, headers);
         // 요청 보내기
         try {
             ResponseEntity<KeywordsBodyDto> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, KeywordsBodyDto.class);
             //ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
-
-            System.out.println(responseEntity.getBody());
-            //return new ResponseEntity<>(HttpStatus.OK);
             return responseEntity;
         } catch (HttpClientErrorException e) {
             // 에러 응답 처리
@@ -71,24 +67,18 @@ public class RequestService {
     }
 
     // 수정필요
-    public ResponseEntity<KeywordsBodyDto> requestSTTKeywords(String text) {
+    public ResponseEntity<KeywordsBodyDto> requestSTTKeywords(String filePath) {
+        int startIndex = filePath.indexOf("application/pdf/") + "application/pdf/".length();
+        String fileName = filePath.substring(startIndex);
+
         RestTemplate restTemplate = new RestTemplate();
         String url = aiUrl + "/keywords";
         // 요청 헤더 추가
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json");
 
-        // 요청 본문 추가
-        //String requestBody = "{\"key\": \"" + filePath + "\", \"db\": \"mysql\"}";
-        String requestBody = """
-                {
-                  "dbInfo": {
-                    "key": "%s",
-                    "db": "mysql"
-                  }
-                }
-                """.formatted(text);
-        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+        RequestBodyToServerDto requestBodyToServerDto = new RequestBodyToServerDto(fileName, "pdf");
+        HttpEntity<RequestBodyToServerDto> requestEntity = new HttpEntity<>(requestBodyToServerDto, headers);
 
         // 요청 보내기
         ResponseEntity<KeywordsBodyDto> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, KeywordsBodyDto.class);
@@ -96,7 +86,41 @@ public class RequestService {
         return responseEntity;
     }
 
-    public String requestPdfSummary(String filePath) throws JsonProcessingException {
+    public ResponseEntity<SummaryDto> requestPdfSummary(String filePath) throws JsonProcessingException, UnsupportedEncodingException {
+        int startIndex = filePath.indexOf("application/pdf/") + "application/pdf/".length();
+        String fileName = filePath.substring(startIndex);
+        System.out.println(fileName);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = aiUrl + "/summary";
+        //String url = "http://echo.jsontest.com/key/value/one/two";
+        // 요청 헤더 추가
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+
+//        // 요청 본문 추가
+//        String requestBody = """
+//                {
+//                  "key": "%s",
+//                  "dataType": "pdf"
+//                }
+//                """.formatted(fileName);
+
+        RequestBodyToServerDto requestBodyToServerDto = new RequestBodyToServerDto(fileName, "pdf");
+        HttpEntity<RequestBodyToServerDto> requestEntity = new HttpEntity<>(requestBodyToServerDto, headers);
+
+        // 요청 보내기
+        ResponseEntity<SummaryDto> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, SummaryDto.class);
+        //ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+//        responseEntity.getBody().
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        JsonNode jsonNode = objectMapper.readTree(responseEntity.getBody());
+//        String text = jsonNode.get("summary").asText();
+
+        return responseEntity;
+    }
+
+        public ResponseEntity<SummaryDto> requestSTTSummary(String filePath) throws JsonProcessingException {
         int startIndex = filePath.indexOf("application/pdf/") + "application/pdf/".length();
         String fileName = filePath.substring(startIndex);
 
@@ -107,60 +131,26 @@ public class RequestService {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json");
 
-        // 요청 본문 추가
-        String requestBody = """
-                {
-                  "dbInfo": {
-                    "key": "%s",
-                    "db": "s3"
-                  }
-                }
-                """.formatted(fileName);
-        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+        RequestBodyToServerDto requestBodyToServerDto = new RequestBodyToServerDto(fileName, "audio");
+        HttpEntity<RequestBodyToServerDto> requestEntity = new HttpEntity<>(requestBodyToServerDto, headers);
 
         // 요청 보내기
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
-        //ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(responseEntity.getBody());
-        String text = jsonNode.get("summary").asText();
-
-        return text;
-    }
-
-    public String requestSTTSummary(String sttResult) throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = aiUrl + "/summary";
-        //String url = "http://echo.jsontest.com/key/value/one/two";
-        // 요청 헤더 추가
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-
-        // 요청 본문 추가
-        String requestBody = """
-                {
-                  "dbInfo": {
-                    "key": "%s",
-                    "db": "mysql"
-                  }
-                }
-                """.formatted(sttResult);
-        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
-
-        // 요청 보내기
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+        ResponseEntity<SummaryDto> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, SummaryDto.class);
         //ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(responseEntity.getBody());
-        String text = jsonNode.get("summary").asText();
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        JsonNode jsonNode = objectMapper.readTree(responseEntity.getBody());
+//        String text = jsonNode.get("summary").asText();
 
         //return responseEntity;
-        return text;
+        return responseEntity;
     }
 
 
-    public ResponseEntity<String> requestPdfMindmap(String filePath, Long documentId, List<String> keywords) throws JsonProcessingException {
+    public ResponseEntity<MindmapResponseDto> requestPdfMindmap(String filePath, Long documentId, List<String> keywords) throws JsonProcessingException {
+        int startIndex = filePath.indexOf("application/pdf/") + "application/pdf/".length();
+        String fileName = filePath.substring(startIndex);
+
         RestTemplate restTemplate = new RestTemplate();
         String url = aiUrl + "/mindmap";
         //String url = "http://echo.jsontest.com/key/value/one/two";
@@ -168,21 +158,23 @@ public class RequestService {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json");
 
-        // 요청 본문 추가
-        String requestBody = """
-                {
-                  "dbInfo": {
-                    "key": "%s",
-                    "db": "s3"
-                  },
-                  "keywords": "%s",
-                  "documentId": %d
-                }
-                """.formatted(filePath, keywords.toString(), documentId);
-        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+//        // 요청 본문 추가
+//        String requestBody = """
+//                {
+//                  "dbInfo": {
+//                    "key": "%s",
+//                    "dataType": "pdf"
+//                  },
+//                  "keywords": "%s",
+//                  "documentId": %d
+//                }
+//                """.formatted(filePath, keywords.toString(), documentId);
+//        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+        MindmapRequestDto mindmapRequestDto = new MindmapRequestDto(fileName, "pdf", keywords, documentId);
+        HttpEntity<MindmapRequestDto> requestEntity = new HttpEntity<>(mindmapRequestDto, headers);
 
         // 요청 보내기
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+        ResponseEntity<MindmapResponseDto> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, MindmapResponseDto.class);
 
         // 마인드맵 mongoDB에 저장
         mindmapService.createMindmap(responseEntity, documentId);
@@ -190,7 +182,10 @@ public class RequestService {
         return responseEntity;
     }
 
-    public ResponseEntity<String> requestSTTMindmap(String stt, Long documentId, List<String> keywords) throws JsonProcessingException {
+    public ResponseEntity<MindmapResponseDto> requestSTTMindmap(String filePath, Long documentId, List<String> keywords) throws JsonProcessingException {
+        int startIndex = filePath.indexOf("application/pdf/") + "application/pdf/".length();
+        String fileName = filePath.substring(startIndex);
+
         RestTemplate restTemplate = new RestTemplate();
         String url = aiUrl + "/mindmap";
         //String url = "http://echo.jsontest.com/key/value/one/two";
@@ -198,28 +193,18 @@ public class RequestService {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json");
 
-        // 요청 본문 추가
-        String requestBody = """
-                {
-                  "dbInfo": {
-                    "key": "%s",
-                    "db": "mysql"
-                  },
-                  "keywords": "%s",
-                  "documentId": %d
-                }
-                """.formatted(stt, keywords.toString(), documentId);
-        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+        MindmapRequestDto mindmapRequestDto = new MindmapRequestDto(fileName, "pdf", keywords, documentId);
+        HttpEntity<MindmapRequestDto> requestEntity = new HttpEntity<>(mindmapRequestDto, headers);
 
         // 요청 보내기
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+        ResponseEntity<MindmapResponseDto> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, MindmapResponseDto.class);
 
         mindmapService.createMindmap(responseEntity, documentId);
 
         return responseEntity;
     }
 
-    public ResponseEntity<String> requestAnswer(Long documentId, String keywordLabel) throws JsonProcessingException {
+    public ResponseEntity<AnswerDto> requestAnswer(Long documentId, String keywordLabel) throws JsonProcessingException {
         Summary summary = summaryService.findSummary(documentId);
         RestTemplate restTemplate = new RestTemplate();
         String url = aiUrl + "/question";
@@ -235,7 +220,7 @@ public class RequestService {
                 """.formatted(summary.getContent(), keywordLabel);
         HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
 
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+        ResponseEntity<AnswerDto> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, AnswerDto.class);
 
         return responseEntity;
     }
