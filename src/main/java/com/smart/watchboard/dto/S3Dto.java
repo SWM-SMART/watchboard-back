5b885e83ad9a15e539df68dc22302acf6cfc8942
kms <noparamin@naver.com>
2023-11-11 23:41:41 +0900

    :hammer: feat: add userId #47

--- src/main/java/com/smart/watchboard/dto/S3Dto.java
+++ src/main/java/com/smart/watchboard/dto/S3Dto.java
@@ -11,11 +11,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class S3Dto {
     private MultipartFile file;
     private Long documentId;
-    //private Long fileId;
+    private Long userId;
+    private String dataType;
 
-    public S3Dto(MultipartFile file, long documentId) {
+    public S3Dto(MultipartFile file, Long documentId, Long userId, String dataType) {
         this.file = file;
         this.documentId = documentId;
-        //this.fileId = fileId;
+        this.userId = userId;
+        this.dataType = dataType;
     }
 }
