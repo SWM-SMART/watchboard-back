bb071c2b9bfaa8afd41f0e1d57b6e80d76f69cdb
kms <noparamin@naver.com>
2023-11-13 16:11:40 +0900

    :hammer: feat: add count pdf pages and check limit  #56

--- src/main/java/com/smart/watchboard/controller/LearningFileController.java
+++ src/main/java/com/smart/watchboard/controller/LearningFileController.java
@@ -1,8 +1,9 @@
 package com.smart.watchboard.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.itextpdf.text.pdf.PdfDocument;
+import com.itextpdf.text.pdf.PdfReader;
 import com.smart.watchboard.common.support.AwsS3Uploader;
-import com.smart.watchboard.domain.File;
 import com.smart.watchboard.dto.*;
 import com.smart.watchboard.service.*;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,9 +16,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
+
+import static com.smart.watchboard.common.support.PdfConverter.checkPageLimit;
+import static com.smart.watchboard.common.support.PdfConverter.countPdfPage;
 
 @RestController
 @RequestMapping("/documents")
@@ -30,11 +32,16 @@ public class LearningFileController {
     private final WhiteboardService whiteboardService;
     private final JwtService jwtService;
     private final SseService sseService;
+    private final RequestService requestService;
 
     @PostMapping("/{documentID}/pdf")
     public ResponseEntity<?> uploadLearningFile(@PathVariable(value = "documentID") long documentId, @RequestParam("pdf") MultipartFile pdfFile, @RequestHeader("Authorization") String accessToken) throws UnsupportedAudioFileException, IOException {
         Optional<Long> id = jwtService.extractUserId(accessToken);
         Long userId = id.orElse(null);
+
+        if (!checkPageLimit(countPdfPage(pdfFile))) {
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
 
         S3Dto s3Dto = new S3Dto(pdfFile, documentId, userId, "pdf");
         String path = awsS3Uploader.uploadFile(s3Dto);
@@ -55,6 +62,10 @@ public class LearningFileController {
     public ResponseEntity<?> updateLearningFile(@PathVariable(value = "documentID") long documentId, @RequestParam("pdf") MultipartFile pdfFile, @RequestHeader("Authorization") String accessToken) throws UnsupportedAudioFileException, IOException {
         Optional<Long> id = jwtService.extractUserId(accessToken);
         Long userId = id.orElse(null);
+
+        if (!checkPageLimit(countPdfPage(pdfFile))) {
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
 
         S3Dto s3Dto = new S3Dto(pdfFile, documentId, userId, "pdf");
         String path = awsS3Uploader.uploadFile(s3Dto);
@@ -85,13 +96,25 @@ public class LearningFileController {
         return responseEntity;
     }
 
-    @GetMapping("/test")
-    public ResponseEntity<?> test(@RequestHeader("Authorization") String accessToken, @RequestParam("pdf") MultipartFile pdfFile) throws JsonProcessingException {
-        Optional<Long> id = jwtService.extractUserId(accessToken);
-        Long userId = id.orElse(null);
+    @PostMapping("/test")
+    public ResponseEntity<?> test(@RequestHeader("Authorization") String accessToken, @RequestParam("pdf") MultipartFile pdfFile) throws IOException {
+//        Optional<Long> id = jwtService.extractUserId(accessToken);
+//        Long userId = id.orElse(null);
+//
+//        S3Dto s3Dto = new S3Dto(pdfFile, 11L, userId, "pdf");
+//        String path = awsS3Uploader.uploadFile(s3Dto);
 
-        S3Dto s3Dto = new S3Dto(pdfFile, 11L, userId, "pdf");
-        String path = awsS3Uploader.uploadFile(s3Dto);
+        String path = "https://watchboard-record-bucket.s3.ap-northeast-2.amazonaws.com/application/pdf/36.감정분류.pdf";
+        ResponseEntity<KeywordsBodyDto> responseEntity = requestService.requestPdfKeywords(path);
+
+        ResponseEntity<MindmapResponseDto> responseEntity1 = requestService.requestPdfMindmap(path, 36L, responseEntity.getBody().getKeywords());
+
+//// PDF 파일 읽기
+//        PdfReader reader = new PdfReader(pdfFile);
+//
+//        // PDF 파일 전체 페이지 수 구하기
+//        int pageCount = reader.getNumberOfPages();
+
         //System.out.println(path);
         //String path = "abcd";
         //ResponseEntity<String> responseEntity = requestService.requestPdfKeywords(path);
@@ -117,7 +140,7 @@ public class LearningFileController {
         //String body = fileService.getPdfUrl(4L);
         //System.out.println(body);
 
-        return new ResponseEntity<>("", HttpStatus.OK);
+        return new ResponseEntity<>(responseEntity1, HttpStatus.OK);
 
     }
 }
