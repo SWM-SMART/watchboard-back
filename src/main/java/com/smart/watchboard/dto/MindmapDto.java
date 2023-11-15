af63cd5ab45fd0e5c13c8edfef170884c2b687cb
kms <noparamin@naver.com>
2023-11-15 20:56:01 +0900

    :hammer: feat: add keywords field #62

--- src/main/java/com/smart/watchboard/dto/MindmapDto.java
+++ src/main/java/com/smart/watchboard/dto/MindmapDto.java
@@ -11,13 +11,13 @@ import java.util.Map;
 public class MindmapDto {
     private int root;
 
-    //private List<String> keywords;
+    private List<String> keywords;
 
     private Map<String, List<Integer>> graph;
 
-    public MindmapDto(int root, Map<String, List<Integer>> graph) {
+    public MindmapDto(int root, List<String> keywords, Map<String, List<Integer>> graph) {
         this.root = root;
-        //this.keywords = keywords;
+        this.keywords = keywords;
         this.graph = graph;
     }
 }
