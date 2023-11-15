a288b200381acf291075fdb44fb8326ca76b66f2
kms <noparamin@naver.com>
2023-11-15 20:57:13 +0900

    :hammer: feat: add SSE in getMindmap #62

--- src/main/java/com/smart/watchboard/controller/GraphController.java
+++ src/main/java/com/smart/watchboard/controller/GraphController.java
@@ -1,6 +1,8 @@
 package com.smart.watchboard.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.smart.watchboard.domain.Document;
+import com.smart.watchboard.domain.Keyword;
 import com.smart.watchboard.dto.AnswerDto;
 import com.smart.watchboard.dto.KeywordsBodyDto;
 import com.smart.watchboard.dto.KeywordsDto;
@@ -19,6 +21,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Tag(name = "그래프 API", description = "그래프 관련 API")
@@ -28,12 +31,12 @@ public class GraphController {
     
     private final LectureNoteService lectureNoteService;
     private final MindmapService mindmapService;
-    private final RequestService requestService;
+    //private final RequestService requestService;
     private final FileService fileService;
     private final WhiteboardService whiteboardService;
     private final SseService sseService;
     private final KeywordService keywordService;
-
+    private final JwtService jwtService;
     @PostMapping("/graph/{documentID}")
     @Operation(summary = "마인드맵 생성", description = "ai 서버에 마인드맵 요청한다.")
     public void createMindmap(@PathVariable(value = "documentID") long documentId, @RequestBody KeywordsBodyDto keywordsBodyDto, @RequestHeader("Authorization") String accessToken) throws JsonProcessingException {
@@ -53,8 +56,29 @@ public class GraphController {
     @GetMapping("/documents/{documentID}/mindmap")
     @Operation(summary = "마인드맵 조회", description = "마인드맵 조회")
     public ResponseEntity<?> getMindmap(@PathVariable(value = "documentID") long documentId, @RequestHeader("Authorization") String accessToken) {
-        MindmapDto mindmapDto = mindmapService.getMindmap(documentId);
+        Optional<Long> id = jwtService.extractUserId(accessToken);
+        Long userId = id.orElse(null);
 
+        MindmapDto mindmapDto = mindmapService.getMindmap(documentId);
+        if (mindmapDto != null) {
+            return new ResponseEntity<>(mindmapDto, HttpStatus.OK);
+        }
+
+        Document document = whiteboardService.findDoc(documentId);
+        if (document.getDataType().equals("pdf")) {
+            String pdfUrl = fileService.getPath(documentId);
+            if (mindmapDto == null) {
+                sseService.notifyKeywords(documentId, pdfUrl);
+                sseService.notifySummary(documentId, pdfUrl);
+                return new ResponseEntity<>(HttpStatus.ACCEPTED);
+            }
+        } else {
+            String url = fileService.getPath(documentId);
+            if (mindmapDto == null) {
+                sseService.notifySTT(documentId, url, userId);
+                return new ResponseEntity<>(HttpStatus.ACCEPTED);
+            }
+        }
         return new ResponseEntity<>(mindmapDto, HttpStatus.OK);
     }
 
@@ -62,7 +86,8 @@ public class GraphController {
     @PutMapping("/documents/{documentID}/mindmap/keyword")
     @Operation(summary = "키워드 업데이트", description = "키워드 추가 및 삭제")
     public ResponseEntity<?> updateKeywords(@PathVariable(value = "documentID") long documentId, @RequestHeader("Authorization") String accessToken, @RequestBody KeywordsDto keywordsDto) {
-        keywordService.updateKeywords(keywordsDto, documentId);
+        Keyword keywords = keywordService.updateKeywords(keywordsDto, documentId);
+        sseService.notify(documentId, keywords.getKeywords());
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -90,8 +115,8 @@ public class GraphController {
     public ResponseEntity<?> test(@PathVariable(value = "documentID") long documentId) throws UnsupportedAudioFileException, IOException {
         System.out.println(documentId);
         if (whiteboardService.isPdfType(documentId)) {
-            String path = fileService.getPdfUrl(documentId);
-            System.out.println(path);
+//            String path = fileService.getPdfUrl(documentId);
+//            System.out.println(path);
             //ResponseEntity<String> body = requestService.requestPdfMindmap(path, documentId, keywords);
             //return new ResponseEntity<>(body, HttpStatus.OK);
         } else if (whiteboardService.isAudioType(documentId)) {
