fe26884664a674242d895ac9ef22bcca51efddb6
kms <noparamin@naver.com>
2023-11-15 20:55:40 +0900

    :hammer: feat: add mindmap check null feature #62

--- src/main/java/com/smart/watchboard/service/MindmapService.java
+++ src/main/java/com/smart/watchboard/service/MindmapService.java
@@ -2,6 +2,7 @@ package com.smart.watchboard.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.smart.watchboard.domain.Document;
+import com.smart.watchboard.domain.Keyword;
 import com.smart.watchboard.domain.Mindmap;
 import com.smart.watchboard.dto.MindmapDto;
 import com.smart.watchboard.dto.MindmapResponseDto;
@@ -21,6 +22,7 @@ public class MindmapService {
 
     private final MindmapRepository mindmapRepository;
     private final WhiteboardService whiteboardService;
+    private final KeywordService keywordService;
 
     public void createMindmap(ResponseEntity<MindmapResponseDto> responseEntity, Long documentId) throws JsonProcessingException {
         Document document = whiteboardService.findDoc(documentId);
@@ -60,7 +62,12 @@ public class MindmapService {
 
     public MindmapDto getMindmap(Long documentId) {
         Optional<Mindmap> mindmap = mindmapRepository.findByDocumentId(documentId);
-        MindmapDto mindmapDto = new MindmapDto(mindmap.get().getRoot(), mindmap.get().getGraph());
+        Keyword keyword = keywordService.findKeywords(documentId);
+
+        if (mindmap.isEmpty()) {
+            return null;
+        }
+        MindmapDto mindmapDto = new MindmapDto(mindmap.get().getRoot(), keyword.getKeywords(), mindmap.get().getGraph());
 
         return mindmapDto;
     }
@@ -68,24 +75,4 @@ public class MindmapService {
     public void deleteMindmap(Long documentId) {
         mindmapRepository.deleteByDocumentId(documentId);
     }
-
-//    public void updateKeywords(KeywordsDto keywordsDto, Long documentId) {
-//        Optional<Mindmap> mindmap = mindmapRepository.findByDocumentId(documentId);
-//        List<String> keywords = mindmap.get().getKeywords();
-//        List<String> addKeywords = keywordsDto.getAdd();
-//        List<String> deleteKeywords = keywordsDto.getDelete();
-//
-//        List<String> newKeywords = new ArrayList<>(keywords);
-//        newKeywords.removeAll(deleteKeywords);
-//        newKeywords.addAll(addKeywords);
-//
-//        Mindmap updatedMindmap = mindmap.orElse(null);
-//        updatedMindmap.setKeywords(newKeywords);
-//
-//        mindmapRepository.save(updatedMindmap);
-//    }
-
-//    public ResponseEntity<String> getKeywordAnswer(Long documentId, String keywordLabel) throws JsonProcessingException {
-//        return requestService.requestAnswer(documentId, keywordLabel);
-//    }
 }
